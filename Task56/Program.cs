4b5a8f8bb19f47fa4118a0a8fe10410d1221e844
«LEONID» <nikonovperm@mail.ru>
2022-04-17 14:00:15 +0500

    Tasks 56,58

---
+++ Task56/Program.cs
@@ -0,0 +1,73 @@
+﻿/* Задача 56: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку 
+с наименьшей суммой элементов. */
+
+int[,] Generate2DArray(int row, int col)
+{
+    int[,] getArray = new int[row,col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            getArray[i,j] = new Random().Next(0,11);
+        }
+    }
+    return getArray;
+}
+
+void PrintArray(int[,] getArray)
+{
+    for (int i = 0; i < getArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < getArray.GetLength(1); j++)
+        {
+            Console.Write($"{getArray[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindArrayStringWithSmallestSumOfElements(int[,] getArray)
+{
+    int sumOfString = 0;
+    int smallestSumOfString = 0;
+    int i = 0;
+    for (int j = 0; j < getArray.GetLength(1); j++)
+    {
+        smallestSumOfString += getArray[i,j];
+    }
+    Console.WriteLine($"Сумма {i} строки {smallestSumOfString}");
+    int numberOfSmallestString = 0;
+
+    for (i = 1; i < getArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < getArray.GetLength(1); j++)
+        {
+            sumOfString += getArray[i,j];
+        }
+        Console.WriteLine($"Сумма {i} строки {sumOfString}");
+        
+        if (sumOfString < smallestSumOfString)
+        {
+            smallestSumOfString = sumOfString;
+            numberOfSmallestString = i;
+        }
+        
+        sumOfString = 0;
+    }
+    Console.WriteLine($"{numberOfSmallestString} строка с наименьшей суммой элементов");
+}
+
+Console.Write("Введите количество строк двухмерного массива: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("Введите количество столбцов двухмерного массива: ");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] getArray = Generate2DArray(row, col);
+PrintArray(getArray);
+Console.WriteLine();
+
+FindArrayStringWithSmallestSumOfElements(getArray);
---
+++ Task58/Program.cs
@@ -0,0 +1,62 @@
+﻿/* Задача 58: Задайте две матрицы. Напишите программу, 
+которая будет находить произведение двух матриц. */
+
+int[,] Generate2DArray(int row, int col)
+{
+    int[,] getArray = new int[row,col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            getArray[i,j] = new Random().Next(0,11);
+        }
+    }
+    return getArray;
+}
+
+void PrintArray(int[,] getArray)
+{
+    for (int i = 0; i < getArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < getArray.GetLength(1); j++)
+        {
+            Console.Write($"{getArray[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ProductOfTwoArrays(int[,] getArray1, int[,] getArray2)
+{
+    int[,] getArray3 = new int[getArray1.GetLength(0), getArray1.GetLength(1)];
+    for (int i = 0; i < getArray1.GetLength(0); i++)
+    {
+        for (int j = 0; j < getArray1.GetLength(1); j++)
+        {
+            getArray3[i,j] = getArray1[i,j] * getArray2[i,j];
+        }
+    }
+    return getArray3;    
+}
+
+Console.Write("Введите количество строк двух двухмерных массивов: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("Введите количество столбцов двух двухмерных массивов: ");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] getArray1 = Generate2DArray(row, col);
+Console.WriteLine("Первый массив:");
+PrintArray(getArray1);
+Console.WriteLine();
+
+int[,] getArray2 = Generate2DArray(row, col);
+Console.WriteLine("Второй массив:");
+PrintArray(getArray2);
+Console.WriteLine();
+
+int[,] getArray3 = ProductOfTwoArrays(getArray1, getArray2);
+Console.WriteLine("Произведение двух массивов:");
+PrintArray(getArray3);
